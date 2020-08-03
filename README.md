49a08399da9d822197f34a436578bbe60498a6aa
Antoine Augusti <antoine.augusti@gmail.com>
2020-08-03 17:00:39 -0400

    Switch to LaTeX

--- README.md
+++ README.md
@@ -8,14 +8,21 @@ See [example.pdf](example.pdf).
 
 ## Installation
 
-Install [pandoc](https://pandoc.org) + [wkhtmltopdf](https://wkhtmltopdf.org/downloads.html). Requires Python 3.8
+Install [pandoc](https://pandoc.org) + LaTeX. You may use a [Docker image](https://hub.docker.com/r/pandoc/latex).
+
+Requires Python 3.8.
 
 ## Usage
 
-For now, run this code in the folder you want to export as PDF.
+Clone in the folder where your documentation folders are.
 
 ```sh
-python3 main.py
-pandoc --toc -t html5 -V papersize=A4 --css=style.css -s guide.md -o guide.pdf
-rm guide.md
+python3 -m venv venv
+source venv/bin/activate
+python main.py src "My documentation title" out/document.pdf
 ```
+
+Arguments are:
+- `src_folder`: the folder where Markdown files are;
+- `title`: title of the document;
+- `output_path`: of the generated PDF file.
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from pathlib import Path
 import re
+import os
+
+import click
+import frontmatter
 
 
-def replace_lines(lines):
+def clean_lines(lines):
     res = list(lines)
 
     pattern = re.compile(r'^::: (tip|warning|danger|lexique)(.*)')
@@ -17,26 +21,85 @@ def replace_lines(lines):
             type, title = [e.strip() for e in m.groups()]
             if type == 'lexique':
                 title = f"Lexique : {title}"
-            res[i] = f"<div class='banner {type}'><p class='banner-title'>{title}</p>\n"
+            res[i] = f"<div class='banner {type}'>\n**{title}**\n\n"
 
         if line.strip() == ':::':
             res[i] = '</div>\n'
 
-        # Hack to remove YAML header content
-        # Horrible code.
-        if line.strip() == '---' or 'permalink:' in line:
-            res[i] = '\n'
-
     return res
 
-p = Path('.')
 
-files = [p / 'README.md'] + sorted(list(p.glob('[0-9]*.md')))
+def build_metadata(title):
+    return {
+        # https://github.com/chdemko/pandoc-latex-environment
+        'pandoc-latex-environment': {
+            'tip': ['banner', 'tip'],
+            'warning': ['banner', 'warning'],
+            'danger': ['banner', 'danger'],
+            'lexique': ['banner', 'lexique'],
+        },
+        # https://github.com/Wandmalfarbe/pandoc-latex-template
+        'titlepage': "true",
+        'toc-own-page': "true",
+        'titlepage-color': "FFFFFF",
+        'titlepage-text-color': "0053b3",
+        'lang': "fr",
+        'title': title,
+        'author': "Etalab",
+        'footer-center': 'Consultez la dernière version de ce guide sur guides.etalab.gouv.fr',
+        'logo': 'logo.png',
+    }
 
-lines = []
-for file in files:
-    with file.open() as f:
-        lines.extend(f.readlines())
 
-with open('guide.md', 'w') as f:
-    f.writelines(replace_lines(lines))
+def file_content(f):
+    md_content = frontmatter.load(f).content
+    lines = f"\n{md_content}\n".split('\n')
+    return [f"{l}\n" for l in lines]
+
+
+@click.command()
+@click.argument('folder', type=click.Path(exists=True))
+@click.argument('title')
+@click.argument('output_path')
+def main(folder, title, output_path):
+    p = Path(folder)
+
+    # Read Markdown content
+    files = [p / 'README.md'] + sorted(list(p.glob('[0-9]*.md')))
+
+    if Path('./images').exists():
+        raise ValueError("images folder already exists. Should not exist for symlinks")
+
+    if len(files) < 2:
+        raise ValueError(f"Did not find Markdown files in {str(p)}")
+
+    lines = []
+    for file in files:
+        with file.open() as f:
+            lines.extend(file_content(f))
+
+    # Create a single file with all content
+    metadata = build_metadata(title)
+    content = frontmatter.Post(''.join(clean_lines(lines)), **metadata)
+    with open('tmp.md', 'w') as f:
+        f.writelines(frontmatter.dumps(content))
+
+    # Build PDF with Pandoc
+    os.makedirs(Path(output_path).parent, exist_ok=True)
+
+    os.symlink(p / 'images/', Path('./images'))
+    os.system(f'''
+    pandoc --toc -s tmp.md -o {output_path} \
+      --template "eisvogel.latex" \
+      --from markdown \
+      --filter pandoc-latex-environment
+    ''')
+
+    # Clean up
+    os.unlink(Path('./images'))
+    os.remove('tmp.md')
+
+    click.echo(f"PDF file has been generated at {output_path}")
+
+if __name__ == '__main__':
+    main()
